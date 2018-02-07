b0246dbd062448518c93f6d6c57a184eced047b2
Chris Spiegel <cspiegel@gmail.com>
2018-02-06 20:47:27 -0800

    Build against (and require) libopenmpt 0.3.

--- mptwrap.cpp
+++ mptwrap.cpp
@@ -39,7 +39,7 @@
 
 MPTWrap::MPTWrap(QIODevice *device)
 {
-  mod = openmpt_module_create(callbacks, device, openmpt_log_func_silent, nullptr, nullptr);
+  mod = openmpt_module_create2(callbacks, device, openmpt_log_func_silent, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr);
   if(mod == nullptr) throw InvalidFile();
 
   openmpt_module_select_subsong(mod, -1);
